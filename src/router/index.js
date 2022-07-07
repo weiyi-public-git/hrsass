8a14e94e142dfd3d37dfb374a3c673b275b39f34
weiyi <cwy76537191@gmail.com>
2022-07-07 18:10:33 +0800

    将静态路由和动态路由临时合并,形成左侧菜单

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+// 引入多个模块的规则
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
 Vue.use(Router)
 
 /* Layout */
@@ -58,14 +66,26 @@ export const constantRoutes = [
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
+// 定义一个动态路由变量
+// 这里导出这个变量 后面做权限的时候会用到
+export const asyncRoutes = [
+  approvalsRouter,
+  departmentsRouter,
+  employeesRouter,
+  permissionRouter,
+  attendancesRouter,
+  salarysRouter,
+  settingRouter,
+  socialRouter
+]
 
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  routes: [...constantRoutes, ...asyncRoutes] // 静态路由和动态路由的临时合并
 })
 
-const router = createRouter()
+const router = createRouter() // 实例化一个路由
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
